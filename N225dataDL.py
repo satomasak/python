d59074a3acb6a498c6743e4be2406bd4ba23dfb2
sato <67639028+satomasak@users.noreply.github.com>
2020-08-28 08:58:11 +0900

    日経先物デリバティブ建玉残高表の自動ダウンロード

--- N225dataDL.py
+++ N225dataDL.py
@@ -5,16 +5,16 @@ import time
 import pandas as pd
 
 
-#chromeのwebdriberのパス
-browser = webdriver.Chrome(executable_path = 'C:\\Users\\sato\\Desktop\\mypandas\\chromedriver.exe')
+#chrome自動操作のためのwebdriberのパス
+page01 = webdriver.Chrome(executable_path = 'C:\\Users\\sato\\Desktop\\mypandas\\chromedriver.exe')
 #待機3秒
-browser.implicitly_wait(3)
+page01.implicitly_wait(3)
 # JPXの当日取引高等のページにアクセス
 url01 = "https://www.jpx.co.jp/markets/derivatives/trading-volume/index.html"
-browser.get(url01)
+page01.get(url01)
 #待機3秒
 time.sleep(3)
 #OSEデリバティブ建玉残高表のダウンロード
-dataDL = browser.find_element_by_xpath('/html/body/div[2]/div[4]/div[1]/div[3]/div[3]/div/table/tbody/tr[2]/td[3]/a')
+dataDL = page01.find_element_by_xpath('/html/body/div[2]/div[4]/div[1]/div[3]/div[3]/div/table/tbody/tr[2]/td[3]/a')
 time.sleep(1)
 dataDL.click()
