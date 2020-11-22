2f6028e56a91ff3746fddd0a4867bdacc799bb77
sato <67639028+satomasak@users.noreply.github.com>
2020-11-22 19:59:49 +0900

    仕入DB追加

--- purchasSellDB.py
+++ purchasSellDB.py
@@ -1,9 +1,12 @@
 #伝票作成システムからエクスポートしたデータをもとに仕入商品ごとの売り上げデータベースを作成する
 
 import pandas as pd
-#sekkdfに必要なカラム番号だけのデータフレームを売上.xlsから抽出する
-selldf = pd.read_excel('C:\\Users\\sato\\Desktop\\purchaseData\\売上.xls',usecols=[1,5,6,9,11,12,15,17,18])
 
+################売上DB作成#####################################
+#selldfに必要なカラム番号だけのデータフレームを売上.xlsから抽出する
+selldf = pd.read_excel('C:\\Users\\sato\\Desktop\\purchaseData\\売上.xls',usecols=[1,5,6,9,11,12,15,17,18])
+#欠損値を’なし’で置き換える
+selldf = selldf.fillna('なし')
 
 #procuctRistの作成　商品一覧　重複を削除し、ユニークデーターを抽出
 productList = selldf[['取引区分','商品名']].drop_duplicates()
@@ -158,3 +161,126 @@ for i in returnedL:
     for b in range(a):
         sh.cell(row=b+1,column=6).number_format = '#,##0'
 wb.save(filePath)
+print('販売データ作成')
+
+#######################売上DB作成以上####################
+
+
+
+#######################仕入DB作成##########################
+buydf = pd.read_excel('C:\\Users\\sato\\Desktop\\purchaseData\\仕入れ.xls',usecols=[1,5,6,9,11,12,15,17,22], skip_blank_lines=False)
+#欠損値を’なし’で置き換える
+buydf = buydf.fillna('なし')
+
+#仕入procuctRistの作成　商品一覧　重複を削除し、ユニークデーターを抽出
+productListP = buydf[['取引区分','商品名']].drop_duplicates()
+#仕入れ一覧の中からトマト、レタスを区分
+PurchaseListT = productListP[productListP['商品名'].str.contains('トマト|日向|ｷｬﾛﾙ|桃姫|ﾄﾏﾄ')]
+PurchaseListL = productListP[productListP['商品名'].str.contains('レタス|ﾚﾀｽ')]
+
+
+#返品以外のトマト商品のリスト
+x = PurchaseListT [~PurchaseListT ['商品名'].str.contains('返品')]
+simpleListBT = x['商品名']
+#返品以外のレタス仕入商品のリスト
+x = PurchaseListL [~PurchaseListL ['商品名'].str.contains('返品')]
+simpleListBL = x['商品名']
+
+#返品リスト
+x = PurchaseListT [PurchaseListT ['商品名'].str.contains('返品')]
+returnedBT = x['商品名']
+#返品以外のレタス仕入商品のリスト
+x = PurchaseListL [PurchaseListL ['商品名'].str.contains('返品')]
+returnedBL = x['商品名']
+
+
+#トマト仕入れ商品の仕入データを商品名、得意先、売上単価ごとにグループ化する(返品以外)
+tomatoBuyData = []
+for i in simpleListBT:
+    nam = buydf[buydf['商品名']== i]
+    #得意先コード、得意先名、商品名、売上単価をキーにして、同じ売上・数量は合計する
+    nam = nam.groupby(['取引年月日','商品ｺｰﾄﾞ','仕入単価','仕入先ｺｰﾄﾞ','仕入先名','行摘要']).sum()
+    tomatoBuyData.append(nam)
+
+#仕入レタスの仕入れデータを商品名、得意先、売上単価ごとにグループ分けする(返品以外)
+lettuceBuyData = []
+for i in simpleListBL:
+    nam = buydf[buydf['商品名']== i]
+    nam = nam.groupby(['取引年月日','商品ｺｰﾄﾞ','仕入単価','仕入先ｺｰﾄﾞ','仕入先名','行摘要']).sum()
+    lettuceBuyData.append(nam)
+
+#トマト売上返品リストの作成
+returnedSimpleListBT =[]
+for i in returnedBT:
+    nam = buydf[buydf['商品名']== i]
+    #得意先コード、得意先名、商品名、売上単価をキーにして、同じ売上・数量は合計する
+    nam = nam.groupby(['取引年月日','商品ｺｰﾄﾞ','仕入単価','仕入先ｺｰﾄﾞ','仕入先名','行摘要']).sum()
+    returnedSimpleListBT.append(nam)
+
+#仕入れレタス売上返品リストの作成
+returnedSimpleListBL =[]
+for i in returnedBL:
+    nam = buydf[buydf['商品名']== i]
+    #得意先コード、得意先名、商品名、売上単価をキーにして、同じ売上・数量は合計する
+    nam = nam.groupby(['取引年月日','商品ｺｰﾄﾞ','仕入単価','仕入先ｺｰﾄﾞ','仕入先名','行摘要']).sum()
+    returnedSimpleListBL.append(nam)
+
+#仕入DB作成
+#トマト仕入データベース
+fileName = dayName + 'トマト仕入データ.xlsx'
+filePath = 'C:\\Users\\sato\\Desktop\\sellData\\'+fileName
+wb = openpyxl.Workbook()
+for i in simpleListBT:
+    wb.create_sheet(i)
+wb.save(filePath)
+j = 0
+with pd.ExcelWriter(filePath, engine='openpyxl') as writer:
+    for i in simpleListBT:
+        tomatoBuyData[j].to_excel(writer, sheet_name=i)
+        j += 1
+
+
+
+#レタス仕入データベース
+fileName = dayName + '仕入レタス仕入データ.xlsx'
+filePath = 'C:\\Users\\sato\\Desktop\\sellData\\'+fileName
+wb = openpyxl.Workbook()
+for i in simpleListBL:
+    wb.create_sheet(i)
+wb.save(filePath)
+j = 0
+with pd.ExcelWriter(filePath, engine='openpyxl') as writer:
+    for i in simpleListBL:
+        lettuceBuyData[j].to_excel(writer, sheet_name=i)
+        j += 1
+
+
+#トマト仕入返品リストの作成
+fileName = dayName + 'トマト仕入返品データ.xlsx'
+filePath = 'C:\\Users\\sato\\Desktop\\sellData\\'+fileName
+wb = openpyxl.Workbook()
+for i in returnedBT:
+    wb.create_sheet(i)
+wb.save(filePath)
+j = 0
+with pd.ExcelWriter(filePath, engine='openpyxl') as writer:
+    for i in returnedBT:
+        returnedSimpleListBT[j].to_excel(writer, sheet_name=i)
+        j += 1
+
+
+
+#レタス仕入返品データベース
+fileName = dayName + '仕入レタス売上返品データ.xlsx'
+filePath = 'C:\\Users\\sato\\Desktop\\sellData\\'+fileName
+wb = openpyxl.Workbook()
+for i in returnedBL:
+    wb.create_sheet(i)
+wb.save(filePath)
+j = 0
+with pd.ExcelWriter(filePath, engine='openpyxl') as writer:
+    for i in returnedBL:
+        returnedSimpleListBL[j].to_excel(writer, sheet_name=i)
+        j += 1
+print('仕入データを作成')
+############################仕入Db作成終了####################
